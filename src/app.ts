30375dd75d481a8259d842b31de34554a5bad109
tatarynrm <tatarynrm@gmail.com>
2025-09-04 18:51:03 +0300

    уцййцууцй

--- src/app.ts
+++ src/app.ts
@@ -10,23 +10,37 @@ import bodyParser from 'body-parser';
 dotenv.config();
 
 const app = express();
-
+const corsOptions = {
+  origin: function (origin: string | undefined, callback: Function) {
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'http://localhost:3005',
+      'https://complex.ict.lviv.ua',
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3005',
-      'https://complex.ict.lviv.ua',
-    ], // адреса твого фронтенда
-    credentials: true, // дозволяє відправляти cookie
-  }),
-);
-
+// app.use(
+//   cors({
+//     origin: [
+//       'http://localhost:3000',
+//       'http://localhost:3005',
+//       'https://complex.ict.lviv.ua',
+//     ], // адреса твого фронтенда
+//     credentials: true, // дозволяє відправляти cookie
+//   }),
+// );
+app.use(cors(corsOptions));
 appRouterState(app);
 export default app;
