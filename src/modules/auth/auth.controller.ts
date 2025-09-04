3413154c8c8374b55c866aaca9d72523a0c52567
tatarynrm <tatarynrm@gmail.com>
2025-09-04 20:14:25 +0300

    qwewqeeqwewq

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -45,15 +45,17 @@ class AuthController {
       }
 
       res.cookie('access_token', result.accessToken, {
-        httpOnly: true,
+        httpOnly: false,
         secure: false,
         sameSite: 'none',
+        path: '/', // дуже важливо! зазвичай треба явно вказати path
         maxAge: 15 * 60 * 1000, // 15 хв
       });
       res.cookie('refresh_token', result.refreshToken, {
-        httpOnly: true,
+        httpOnly: false,
         secure: false,
         sameSite: 'none',
+        path: '/', // дуже важливо! зазвичай треба явно вказати path
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 днів
       });
       res.json(result);
@@ -75,9 +77,10 @@ class AuthController {
 
       if (newAccessToken.accessToken) {
         res.cookie('access_token', newAccessToken.accessToken, {
-          httpOnly: true,
+          httpOnly: false,
           secure: false,
           sameSite: 'none',
+          path: '/', // дуже важливо! зазвичай треба явно вказати path
           maxAge: 15 * 60 * 1000, // 15 хв
         });
 
